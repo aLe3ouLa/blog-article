03426dcd5c8b661ea54454ce7ada29a888d3f8c4
Alexandra <ale3mp90@gmail.com>
2019-11-26 17:41:29 +0100

    Adjust send button

--- js/main.js
+++ js/main.js
@@ -1,71 +1,62 @@
-let button = document.getElementById('btnSend');
-
-button.addEventListener('click', function (event) {
-    event.preventDefault();
+function sendButton() {
     let textarea = document.getElementById('newComment');
-    let html = `<div class="conversation">
-    <div class="reply u-margin-bottom-small">
-        <div class="reply__details">
-            <div class="reply__details__wrapper">
-                <img class="reply__details__wrapper--avatar"
-                    src="./assets/E2ADFD8B-EB2E-4DE9-9319-10887B04FAEC.png" alt="">
-                <div class="reply__details__wrapper_text">
-                    <span class="reply__details__wrapper_text--name">Patrícia Ribeiro</span>
-                    <span class="reply__details__wrapper_text--date">February 2, 2019, 3:05
-                        PM</span>
-                </div>
-            </div>
-            <div class="dropdown">
-                <span class="fas fa-ellipsis-v"></span>
-                <ul class="dropdown-content">
-                    <li> <span class="fas fa-pen"></span> &nbsp; Edit</li>
-                    <li><span class="fas fa-trash"></span> &nbsp; Delete</li>
-                </ul>
-            </div>
-        </div>
-        <div class="reply__content u-margin-bottom-xsm ">${textarea.value}</div>
-        <div class="reply__actions">
-            <div class="reply__actions--like">
-                <span class="far fa-heart"></span>
-                <span class="text">Like</span>
-            </div>
-            <hr class="vertical">
-            <div class="reply__actions--reply">
-                <span class="far fa-comment"></span>
-                <span class="text">reply</span>
-            </div>
-        </div>
-
-        <div id="inline-reply" class="reply__inline_reply">
-            <div class="comment__textarea">
-                <img class="comment__textarea--avatar"
-                    src="./assets/E2ADFD8B-EB2E-4DE9-9319-10887B04FAEC.png" alt="">
-                <div class="comment__textarea--textarea">
-                    <div class="comment__textarea--textarea--container">
-                        <textarea id="newComment" name="newComment"
-                            placeholder="Write a comment..." rows="10"></textarea>
+    var now = moment(new Date).format("MMMM D, YYYY, h:mm a");
+    if (textarea.value !== '') {
+        let html = `<div class="conversation">
+        <div class="reply u-margin-bottom-small">
+            <div class="reply__details">
+                <div class="reply__details__wrapper">
+                    <img class="reply__details__wrapper--avatar"
+                        src="./assets/E2ADFD8B-EB2E-4DE9-9319-10887B04FAEC.png" alt="">
+                    <div class="reply__details__wrapper_text">
+                        <span class="reply__details__wrapper_text--name u-margin-bottom-xxsm">Patrícia Ribeiro</span>
+                        <span class="reply__details__wrapper_text--date">${ now }</span>
                     </div>
-                    <button id="btnSend" class="btn">Send</button>
+                </div>
+                <div class="dropdown">
+                    <span class="fas fa-ellipsis-v"></span>
+                    <ul class="dropdown-content">
+                        <li> <span class="fas fa-pen"></span> &nbsp; Edit</li>
+                        <li><span class="fas fa-trash"></span> &nbsp; Delete</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="reply__content u-margin-bottom-xsm ">${textarea.value}</div>
+            <div class="reply__actions">
+                <div class="reply__actions--like" onclick="clickHeart(this)">
+                    <span class="far fa-heart"></span>
+                    <span class="text">Like</span>
+                </div>
+                <hr class="vertical">
+                <div class="reply__actions--reply" onclick="clickReply(this)">
+                    <span class="far fa-comment"></span>
+                    <span class="text">reply</span>
+                </div>
+            </div>
+
+            <div id="inline-reply" class="reply__inline_reply">
+                <div class="comment__textarea">
+                    <img class="comment__textarea--avatar"
+                        src="./assets/E2ADFD8B-EB2E-4DE9-9319-10887B04FAEC.png" alt="">
+                    <div class="comment__textarea--textarea">
+                        <div class="comment__textarea--textarea--container">
+                            <textarea id="newComment" name="newComment"
+                                placeholder="Write a comment..." rows="10"></textarea>
+                        </div>
+                        <button id="btnSendInlineReply" class="btn">Send</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="inline-replies-container"></div>
-</div>`
+        <div class="inline-replies-container"></div>
+    </div>`;
 
-    let parentDiv = document.getElementById('replies-container');
-    parentDiv.innerHTML = html + parentDiv.innerHTML;
-
-    textarea.value = null;
-
-    hearts();
-
-    replies();
-
-}, false);
-
-
+        let parentDiv = document.getElementById('replies-container');
+        parentDiv.innerHTML = html + parentDiv.innerHTML;
+        textarea.value = null;
+    }
+}
 
 function showHidden() {
     let hidden = document.getElementById("hidden-conv");
